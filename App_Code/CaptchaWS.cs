c2f0ef8aa81944754112eed39d28ea0423964e97
Mina Yousseif <mina.yousseif@gmail.com>
2014-11-23 13:45:47 -0500

    captcha web service

---
+++ App_Code/CaptchaWS.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using SimpleCaptcha;
+
+/// <summary>
+/// Summary description for CaptchaWS
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+ [System.Web.Script.Services.ScriptService]
+public class CaptchaWS : System.Web.Services.WebService {
+
+    public CaptchaWS () {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod(EnableSession = true)]
+    public string GenCaptcha() {
+        return captcha.GenCaptcha();
+    }
+    
+}
---
+++ App_Code/captcha.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for captcha
+/// </summary>
+namespace SimpleCaptcha
+{
+    public class captcha
+    {
+        public static string GenCaptcha(int _clength = 5)
+        {
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var random = new Random();
+            string captcha = new string(
+                Enumerable.Repeat(chars, _clength)
+                          .Select(s => s[random.Next(s.Length)])
+                          .ToArray());
+            HttpContext.Current.Session["captcha"] = captcha;
+            return captcha;
+        }
+    }
+}
